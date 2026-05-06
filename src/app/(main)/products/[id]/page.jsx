7c3ef6c40b8e2b8c1b3277525e94b34ff96babd3
saranciucnichita <saranciucnichita@gmail.com>
2026-05-06 22:44:06 +0300

    Update dependencies and fix scrap script

--- src/app/(main)/products/[id]/page.jsx
+++ src/app/(main)/products/[id]/page.jsx
@@ -5,36 +5,37 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 
 export default function ProductPage() {
-  const [item, setItem] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const params = useParams();
+    const [item, setItem] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const params = useParams();
 
-  useEffect(() => {
-      const fetchData = async () => {
-          try {
-              const res = await fetch(`https://tasteful-angel-9db2a6bb3b.strapiapp.com/api/items?filters[id][$eq]=${params.id}&populate=*`);
-              const response = await res.json();
-              if (!response.ok) throw new Error('Network response was not ok');
-              setItem(response.data || null);
-          } catch (error) {
-              console.error("Failed to fetch: ", error);
-          } finally {
-              setLoading(false);
-          }
-      };
-      fetchData();
-  }, [params.id]);
+    useEffect(() => {
+        const fetchData = async() => {
+            const { id } = await params;
+            if (!id) return;
+            try {
+                const res = await fetch(`https://tasteful-angel-9db2a6bb3b.strapiapp.com/api/items?filters[id][$eq]=${id}&populate=*`);
+                const response = await res.json();
+                setItem(response.data[0] || null);
+            } catch (error) {
+                console.error("Failed to fetch: ", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, [params]);
 
-  return (
-            (!loading && item) ? (
+    return (
+        (!loading && item) ? (
             <div>
                 <h1>{item.Name}</h1>
                 <p>{item.Brand} - {item.Model}</p>
             </div>
-          ) : (
-          <Box sx={{ display: 'flex' }}>
-            <CircularProgress aria-label="Loading…" />
-          </Box>
-      )
-  );
+        ) : (
+            <Box sx={{ display: 'flex', padding: '10px' }}>
+                <CircularProgress aria-label="Loading…" />
+            </Box>
+        )
+    );
 }
